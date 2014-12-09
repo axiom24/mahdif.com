d4ad7e71a3256c820f56fb7d8f70e52803cea3dd
Mahdi <me@mahdif.com>
2014-12-09 17:20:14 +0100

    First commit for all the files

---
+++ sandbox/Text_Counter/js/script.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+	document.getElementById('txtWriteHere').onclick = function() {
+		if (document.getElementById('txtWriteHere').value === "...") {
+			document.getElementById('txtWriteHere').value = "";
+		}
+	};
+	
+	document.getElementById('txtWriteHere').onblur = function(){
+		if (document.getElementById('txtWriteHere').value === ""){
+			document.getElementById('txtWriteHere').value = "...";
+		}
+	};
+
+	function lettersCountNumb(){
+		return document.getElementById('txtWriteHere').value.length;
+	}
+	
+	document.getElementById('txtWriteHere').onkeyup = function() {
+			document.getElementById('titleKeyCountNum').innerHTML = lettersCountNumb();
+	};
+};
